f10d4b2cadfa8b5b1fcd7f40a9f0f6b7143ca96a
shaheerkhan12 <khanshaheer076@gmail.com>
2020-09-25 12:12:33 +0500

    latest commit routing 101

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
+import { AppComponent } from "./app.component";
+import { SubcomponentComponent } from "./subcomponent/subcomponent.component";
 
+const routes: Routes = [
+  { path: "dummy", component: AppComponent },
 
-const routes: Routes = [];
+  { path: "", redirectTo: "/dummy", pathMatch: "full" },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SubcomponentComponent } from './subcomponent/subcomponent.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SubcomponentComponent
   ],
   imports: [
     BrowserModule,
